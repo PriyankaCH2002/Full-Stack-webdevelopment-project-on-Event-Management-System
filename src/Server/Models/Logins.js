5ee83327d58752a48ff540d8dc1812053ac31210
PriyankaCH2002 <156978864+PriyankaCH2002@users.noreply.github.com>
2024-03-30 16:16:15 +0530

    Delete src/Server directory

--- src/Server/Models/Logins.js
+++
@@ -1,9 +0,0 @@
-const mongoose= require('mongoose')
-
-const LoginSchema= new mongoose.Schema({
-    username:String,
-    password:String
-}) 
-
-const LoginDetails= mongoose.model("Login", LoginSchema)
-module.exports=LoginDetails;
